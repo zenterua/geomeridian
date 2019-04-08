41ff4ec3be5336f379c6f865c7d1e8055951f851
Zinoviy <forwebmasterua@gmail.com>
2019-04-08 18:59:32 +0300

    add filter functionality

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 jQuery(function ($) {
     'use strict';
-    var map, markers = [], snazzyWindow, markersClusters, clusterStyles, previousPopup, centerLatlng, mapOptions, styles, styledMap;
+    var map, markers = [], snazzyWindow, markersClusters, clusterStyles, previousPopup, centerLatlng, mapOptions, styles, styledMap, geocoder, geoPlaceLatLng, circle, allPlaces;
 
     function addMarker(lat, lng, type, name, popupContent) {
         var markerImage = {
@@ -10,30 +10,33 @@ jQuery(function ($) {
 
         var markerLatLng = new google.maps.LatLng(lat,lng);
 
-        markers[name] = new google.maps.Marker({
+        var singleMarker = new google.maps.Marker({
             position: markerLatLng,
             icon: markerImage,
             name: name,
-            content: popupContent,
+            content: popupContent
         });
-        markers[name].setMap(map);
 
-        google.maps.event.addListener(markers[name], 'click', function(){
+        markers.push(singleMarker);
+
+        google.maps.event.addListener(singleMarker, 'click', function(){
             showPreviosMarker();
-            showInfowindow(markers[name]);
-            previousPopup = markers[name];
-            markers[name].setMap(null);
+            showInfowindow(this);
+            previousPopup = this;
+            this.setMap(null);
 
             setTimeout(function () {
                 $('.shortcode-type10').addClass('show');
             },100);
         });
 
+        singleMarker.setMap(map);
+
     }
 
     function initMap() {
         centerLatlng = new google.maps.LatLng($('#map').attr("data-lat"),$('#map').attr("data-lng"));
-
+        geocoder = new google.maps.Geocoder();
         mapOptions = {
             zoom: parseInt($('#map').attr("data-zoom")),
             zoomControl: true,
@@ -71,6 +74,17 @@ jQuery(function ($) {
             }
         ];
 
+        if (window.navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var location = {lat: position.coords.latitude, lng: position.coords.longitude};
+                geocoder.geocode({'latLng': location}, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK && results[0]) {
+                        geoPlaceLatLng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+                    }
+                });
+            });
+        }
+
         // Ajax. get map marker info
         $.ajax({
             url: 'mapLocations.json',
@@ -80,15 +94,8 @@ jQuery(function ($) {
                 console.log(error)
             },
             success: function (response) {
+                allPlaces = response;
                 response.forEach(function(country) { // Create country marker
-                    addMarker(country.coordinates.lat, country.coordinates.lng, country.type, country.name, country.popupContent);
-
-                    if ( country.places ) {
-                        country.places.forEach(function (capitalPlace) {
-                            addMarker(capitalPlace.coordinates.lat, capitalPlace.coordinates.lng, capitalPlace.type, capitalPlace.name, capitalPlace.popupContent);
-                        })
-                    }
-
                     if ( country.city ) {
                         country.city.forEach(function(city) { // Find inside country city and create marker city
                             addMarker(city.coordinates.lat, city.coordinates.lng, city.type, city.name, city.popupContent);
@@ -119,11 +126,137 @@ jQuery(function ($) {
         if ( previousPopup ) previousPopup.setMap(map);
     }
 
+    function closestPlaces(geoPlaceLatLng) {
+        map.setCenter(geoPlaceLatLng);
+        map.setZoom(10);
+
+        if ( circle ) circle.setMap(null);
+
+         circle = new google.maps.Circle({
+            strokeColor: '#F7E8E3',
+            strokeOpacity: 1,
+            strokeWeight: 10,
+            fillColor: '#F4AE4C',
+            fillOpacity: .5,
+            map: map,
+            center: geoPlaceLatLng,
+            radius: $('.closestPlaces').data('search-radius') ? +$('.closestPlaces').data('search-radius') : 1000
+        });
+    }
+
+    function getBounds() {
+        var bounds = new google.maps.LatLngBounds();
+
+        markers.forEach(function (marker) {
+            bounds.extend(marker.getPosition());
+        });
+
+        map.fitBounds(bounds);
+    }
+
+    function filtering(filterCountry, filterType, filterCity, filterCategory) {
+        //clear markers and clusters
+        snazzyWindow.close();
+        markers.forEach(function (i) {
+            i.setMap(null);
+        });
+        markers = [];
+        if ( markersClusters ) markersClusters.clearMarkers();
+        if ( circle ) circle.setMap(null);
+
+        if ( filterType.toLowerCase() === 'country' ) { // show all county city and places
+            allPlaces.forEach(function (country) {
+                if ( country.name === filterCountry ) {
+
+                    if ( country.city ) {
+                        country.city.forEach(function(city) { // Find inside country city and create marker city
+                            addMarker(city.coordinates.lat, city.coordinates.lng, city.type, city.name, city.popupContent);
+
+                            if ( city.places ) {
+                                city.places.forEach(function(place) { // Inside city find places and create places marker
+                                    addMarker(place.coordinates.lat, place.coordinates.lng, place.type, place.name, place.popupContent);
+                                })
+                            }
+
+                        })
+                    }
+
+                }
+            });
+            getBounds();
+            markersClusters = new MarkerClusterer(map, markers, {styles: clusterStyles});
+        }
+
+        if ( filterType.toLowerCase() === 'city' ) {
+            allPlaces.forEach(function (country) {
+                if ( country.name === filterCountry ) {
+                    if ( country.city ) {
+                        country.city.forEach(function(city) { // Find inside country city and create marker city
+                            console.log(city, filterCity);
+                            if ( city.name === filterCity ) {
+                                addMarker(city.coordinates.lat, city.coordinates.lng, city.type, city.name, city.popupContent);
+
+                                if ( city.places ) {
+                                    city.places.forEach(function(place) { // Inside city find places and create places marker
+                                        addMarker(place.coordinates.lat, place.coordinates.lng, place.type, place.name, place.popupContent);
+                                    })
+                                }
+                            }
+                        });
+                        getBounds();
+                        markersClusters = new MarkerClusterer(map, markers, {styles: clusterStyles});
+                    }
+                }
+            });
+        }
+
+        if ( filterType.toLowerCase() === 'category' ) {
+            allPlaces.forEach(function (country) {
+                if ( country.name === filterCountry ) {
+                    if ( country.city ) {
+                        country.city.forEach(function(city) { // Find inside country city and create marker city
+                            if ( city.name === filterCity ) {
+
+                                if ( city.places ) {
+                                    city.places.forEach(function(place) { // Inside city find places and create places marker
+                                        console.log( place.category, filterCategory );
+                                        if ( place.category === filterCategory ) {
+                                            addMarker(place.coordinates.lat, place.coordinates.lng, place.type, place.name, place.popupContent);
+                                        }
+                                    })
+                                }
+
+                            }
+                        });
+                        getBounds();
+                        markersClusters = new MarkerClusterer(map, markers, {styles: clusterStyles});
+                    }
+                }
+            });
+        }
+    }
 
     setTimeout(function () {
         initMap();
 
-    },500)
+        $(document).on('click', '.closestPlaces', function () {
+            closestPlaces();
+        });
+
+        $(document).on('click', '.mapFilter.country', function () {
+            console.log('country');
+            filtering( $(this).data('filter-country'), $(this).data('filter-type') );
+        });
+
+        $(document).on('click', '.mapFilter.city', function () {
+            filtering( $(this).data('filter-country'), $(this).data('filter-type'), $(this).data('filter-city') );
+        });
+
+        $(document).on('click', '.mapFilter.category', function () {
+            filtering( $(this).data('filter-country'), $(this).data('filter-type'), $(this).data('filter-city'), $(this).data('filter-category') );
+        });
+
+    },500);
 
 });
 
@@ -134,4 +267,8 @@ function showPopupShare() {
         document.querySelector('.shortcode-type10').classList.add('showShare');
     }
 
+}
+
+function closeSharePopup() {
+    document.querySelector('.showShare').classList.remove('showShare');
 }
